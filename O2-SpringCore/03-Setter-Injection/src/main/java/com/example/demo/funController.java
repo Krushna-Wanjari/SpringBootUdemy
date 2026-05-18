6a85da6aabc3e16089a656d7969213da84454001
Krushna-Wanjari <krushnawanjari8@gmail.com>
2026-05-18 16:55:24 +0530

    Added Setter Injection project

---
+++ O2-SpringCore/03-Setter-Injection/src/main/java/com/example/demo/funController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class funController {
+    private coach s;
+
+    @Autowired
+    public void setCoach(coach thecoach) {
+        this.s = thecoach;
+    }
+
+    @GetMapping("/coach")
+    public String getCoach() {
+        return s.getCoach();
+    }
+}
